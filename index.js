0ce734f3cf4365f31dd5109d9c379c05108cad52
이민욱 <plainxuk@plainxuk-MacBook-Pro.local>
2022-04-19 17:16:34 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://lucas:0800@cluster0.ad661.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!, 안녕하세요!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
